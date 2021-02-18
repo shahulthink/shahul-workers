b60f8a354035fbf071ddcf07773d63fedf7764b4
shahulHameed <shahul.hameed@thinktower.in>
2021-02-18 16:52:15 +0530

    update thesite

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null)
+  const fetchURL = "https://jsonplaceholder.typicode.com"
+  const getData = () =>
+    fetch(`${fetchURL}/posts`)
+      .then((res) => res.json())
+
+  useEffect(() => {
+    getData().then((data) => setData(data))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      {data?.map((item) => 
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+       <ul className="border border-gray-200 rounded-md divide-y divide-gray-200">
+           <li className="pl-3 pr-4 py-3 flex items-center justify-between text-sm">{item.title}</li>
+        </ul>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
